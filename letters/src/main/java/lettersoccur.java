5c7a6219e01a10ecd385cf3ecb6eec754a75b03c
Hasara Ismini <cst17008@std.uwu.ac.lk>
2022-03-07 23:53:48 +0530

    Occurrence of Letters

---
+++ letters/src/main/java/lettersoccur.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+
+public class lettersoccur {
+    public static void main(String[] args) {
+        int i, length, counter[] = new int[256];
+ 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your Sentence:");
+        String text01= scanner.nextLine();
+       
+        String text02=text01.toLowerCase();
+
+        String text03;
+        text03 = text02.replaceAll(" ", "");
+        length = text03.length();
+       
+        
+        for (i = 0; i < length; i++) {
+            counter[(int) text03.charAt(i)]++;
+        }
+        
+        for (i = 0; i < 256; i++) {
+            if (counter[i] != 0) {
+                System.out.println((char) i + " - " + counter[i]);
+            }
+        }
+    } 
+}
