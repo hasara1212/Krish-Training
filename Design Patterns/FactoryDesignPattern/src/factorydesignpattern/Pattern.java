467fbae8a67bef5d5d3986e00302bdcee822c4d6
Hasara Ismini <cst17008@std.uwu.ac.lk>
2022-03-08 16:16:00 +0530

    Design Patterns

---
+++ Design Patterns/FactoryDesignPattern/src/factorydesignpattern/Pattern.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package factorydesignpattern;
+
+/**
+ *
+ * @author HASA
+ */
+public class Pattern implements ScreenLock {
+
+    @Override
+    public void screenlocktype() {
+        System.out.println("your Lock Screen Pattern Entering is Working. "); 
+    }
+    
+}
